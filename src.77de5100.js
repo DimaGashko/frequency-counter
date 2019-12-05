c902f6a2a8ae5167b259b79e32fe99dd1ac2276f
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-05 20:26:33 +0200

    base layout

--- src.77de5100.js
+++ src.77de5100.js
@@ -380,7 +380,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34553" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36373" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

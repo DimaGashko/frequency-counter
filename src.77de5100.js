425e5984bd4a43654ca63635124c9675d07a0849
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-06 00:29:51 +0200

    editor

--- src.77de5100.js
+++ src.77de5100.js
@@ -327,19 +327,168 @@ function prepareText(text, options) {
 
   return text;
 }
-},{"./_calc-frequency":"scripts/algorithms/char-frequency/_calc-frequency.ts"}],"scripts/editor.ts":[function(require,module,exports) {
+},{"./_calc-frequency":"scripts/algorithms/char-frequency/_calc-frequency.ts"}],"../node_modules/throttle-debounce/dist/index.esm.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.throttle = throttle;
+exports.debounce = debounce;
+
+/* eslint-disable no-undefined,no-param-reassign,no-shadow */
+
+/**
+ * Throttle execution of a function. Especially useful for rate limiting
+ * execution of handlers on events like resize and scroll.
+ *
+ * @param  {Number}    delay          A zero-or-greater delay in milliseconds. For event callbacks, values around 100 or 250 (or even higher) are most useful.
+ * @param  {Boolean}   [noTrailing]   Optional, defaults to false. If noTrailing is true, callback will only execute every `delay` milliseconds while the
+ *                                    throttled-function is being called. If noTrailing is false or unspecified, callback will be executed one final time
+ *                                    after the last throttled-function call. (After the throttled-function has not been called for `delay` milliseconds,
+ *                                    the internal counter is reset)
+ * @param  {Function}  callback       A function to be executed after delay milliseconds. The `this` context and all arguments are passed through, as-is,
+ *                                    to `callback` when the throttled-function is executed.
+ * @param  {Boolean}   [debounceMode] If `debounceMode` is true (at begin), schedule `clear` to execute after `delay` ms. If `debounceMode` is false (at end),
+ *                                    schedule `callback` to execute after `delay` ms.
+ *
+ * @return {Function}  A new, throttled, function.
+ */
+function throttle(delay, noTrailing, callback, debounceMode) {
+  /*
+   * After wrapper has stopped being called, this timeout ensures that
+   * `callback` is executed at the proper times in `throttle` and `end`
+   * debounce modes.
+   */
+  var timeoutID;
+  var cancelled = false; // Keep track of the last time `callback` was executed.
+
+  var lastExec = 0; // Function to clear existing timeout
+
+  function clearExistingTimeout() {
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+  } // Function to cancel next exec
+
+
+  function cancel() {
+    clearExistingTimeout();
+    cancelled = true;
+  } // `noTrailing` defaults to falsy.
+
+
+  if (typeof noTrailing !== 'boolean') {
+    debounceMode = callback;
+    callback = noTrailing;
+    noTrailing = undefined;
+  }
+  /*
+   * The `wrapper` function encapsulates all of the throttling / debouncing
+   * functionality and when executed will limit the rate at which `callback`
+   * is executed.
+   */
+
+
+  function wrapper() {
+    var self = this;
+    var elapsed = Date.now() - lastExec;
+    var args = arguments;
+
+    if (cancelled) {
+      return;
+    } // Execute `callback` and update the `lastExec` timestamp.
+
+
+    function exec() {
+      lastExec = Date.now();
+      callback.apply(self, args);
+    }
+    /*
+     * If `debounceMode` is true (at begin) this is used to clear the flag
+     * to allow future `callback` executions.
+     */
+
+
+    function clear() {
+      timeoutID = undefined;
+    }
+
+    if (debounceMode && !timeoutID) {
+      /*
+       * Since `wrapper` is being called for the first time and
+       * `debounceMode` is true (at begin), execute `callback`.
+       */
+      exec();
+    }
+
+    clearExistingTimeout();
+
+    if (debounceMode === undefined && elapsed > delay) {
+      /*
+       * In throttle mode, if `delay` time has been exceeded, execute
+       * `callback`.
+       */
+      exec();
+    } else if (noTrailing !== true) {
+      /*
+       * In trailing throttle mode, since `delay` time has not been
+       * exceeded, schedule `callback` to execute `delay` ms after most
+       * recent execution.
+       *
+       * If `debounceMode` is true (at begin), schedule `clear` to execute
+       * after `delay` ms.
+       *
+       * If `debounceMode` is false (at end), schedule `callback` to
+       * execute after `delay` ms.
+       */
+      timeoutID = setTimeout(debounceMode ? clear : exec, debounceMode === undefined ? delay - elapsed : delay);
+    }
+  }
+
+  wrapper.cancel = cancel; // Return the wrapper function.
+
+  return wrapper;
+}
+/* eslint-disable no-undefined */
+
+/**
+ * Debounce execution of a function. Debouncing, unlike throttling,
+ * guarantees that a function is only executed a single time, either at the
+ * very beginning of a series of calls, or at the very end.
+ *
+ * @param  {Number}   delay         A zero-or-greater delay in milliseconds. For event callbacks, values around 100 or 250 (or even higher) are most useful.
+ * @param  {Boolean}  [atBegin]     Optional, defaults to false. If atBegin is false or unspecified, callback will only be executed `delay` milliseconds
+ *                                  after the last debounced-function call. If atBegin is true, callback will be executed only at the first debounced-function call.
+ *                                  (After the throttled-function has not been called for `delay` milliseconds, the internal counter is reset).
+ * @param  {Function} callback      A function to be executed after delay milliseconds. The `this` context and all arguments are passed through, as-is,
+ *                                  to `callback` when the debounced-function is executed.
+ *
+ * @return {Function} A new, debounced function.
+ */
+
+
+function debounce(delay, atBegin, callback) {
+  return callback === undefined ? throttle(delay, atBegin, false) : throttle(delay, callback, atBegin !== false);
+}
+},{}],"scripts/editor.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var throttle_debounce_1 = require("throttle-debounce");
+
 var Editor =
 /** @class */
 function () {
   function Editor(root) {
     this.root = root;
+    this.MAX_HIGHLIGHT_LEN = 10000;
+    this.MAX_LIVE_HIGHLIGHT_LEN = 2000;
     this._value = '';
+    this._prevDecTextValue = '';
     this.$ = {};
     this._decTextUpdatingFrame = 0;
     this.init();
@@ -354,11 +503,33 @@ function () {
   Editor.prototype.initEvents = function () {
     var _this = this;
 
-    this.$.realText.addEventListener('keydown', function () {
+    this.$.realText.addEventListener('input', function () {
+      if (_this._value.length > _this.MAX_LIVE_HIGHLIGHT_LEN) {
+        return;
+      }
+
       _this.startDecTextUpdating();
+
+      setTimeout(function () {
+        return _this.stopDecTextUpdating();
+      }, 1000);
     });
-    this.$.realText.addEventListener('keyup', function () {
+    this.$.realText.addEventListener('input', throttle_debounce_1.throttle(300, function () {
+      if (_this._value.length > _this.MAX_LIVE_HIGHLIGHT_LEN) {
+        _this.updateDecTextScrolls();
+
+        _this.readRealText();
+      }
+    }));
+    this.$.realText.addEventListener('keyup', throttle_debounce_1.throttle(300, function () {
       _this.stopDecTextUpdating();
+
+      _this.updateDecTextScrolls();
+
+      _this.readRealText();
+    }));
+    this.$.realText.addEventListener('scroll', function () {
+      _this.updateDecTextScrolls();
     });
   };
   /** Start automatic dec text updating */
@@ -368,7 +539,6 @@ function () {
     if (this._decTextUpdatingFrame) return;
     var editor = this;
     this._decTextUpdatingFrame = requestAnimationFrame(function tik() {
-      console.log('hi');
       editor.readRealText();
       editor._decTextUpdatingFrame = requestAnimationFrame(tik);
     });
@@ -384,8 +554,23 @@ function () {
     this.updateDecText();
   };
 
+  Editor.prototype.updateDecTextScrolls = function () {
+    this.$.decText.scrollTop = this.$.realText.scrollTop;
+    this.$.decText.scrollLeft = this.$.realText.scrollLeft;
+  };
+
   Editor.prototype.updateDecText = function () {
-    this.$.decText.innerText = this._value;
+    if (this._prevDecTextValue === this._value) return;
+    this._prevDecTextValue = this._value;
+
+    if (this._value.length > this.MAX_HIGHLIGHT_LEN) {
+      this.$.decText.innerHTML = this._value;
+      return;
+    }
+
+    this.$.decText.innerHTML = this._value.split('').map(function (item, i) {
+      return i % 2 ? "<span style=\"color: red\">" + item + "</span>" : item;
+    }).join('');
   };
 
   Editor.prototype.initValue = function () {
@@ -402,7 +587,7 @@ function () {
 }();
 
 exports.default = Editor;
-},{}],"index.ts":[function(require,module,exports) {
+},{"throttle-debounce":"../node_modules/throttle-debounce/dist/index.esm.js"}],"index.ts":[function(require,module,exports) {
 "use strict";
 
 var __importDefault = this && this.__importDefault || function (mod) {

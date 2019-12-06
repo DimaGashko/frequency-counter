284be73e6c2976ec43f813c7639d1c79fe16f3f2
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-07 00:32:52 +0200

    move to Res

--- src/scripts/Res.ts
+++ src/scripts/Res.ts
@@ -1,3 +1,4 @@
+import Chart from 'chart.js';
 import Frequency from "./Frequency";
 
 export default class Res {
@@ -6,6 +7,9 @@ export default class Res {
 
    charCtx: CanvasRenderingContext2D;
    pairCtx: CanvasRenderingContext2D;
+
+   charChart: Chart;
+   pairChart: Chart;
 
    $: { [name: string]: HTMLElement } = {};
 
@@ -39,11 +43,41 @@ export default class Res {
    }
 
    private updateCharChart() {
-      
+      const entries = Array.from(this.frequency.char.map.entries());
+      entries.sort((a, b) => b[1] - a[1]);
+
+      const labels = entries.map(e => e[0]);
+      const data = entries.map(e => e[1]).map(v => this.formatValue(v));
+
+      if (this.charChart) {
+         this.charChart.data.labels = labels;
+         this.charChart.data.datasets[0].data = data;
+         this.charChart.update();
+         return;
+      }
+
+      this.charChart = new Chart(this.charCtx, {
+         type: 'bar',
+         data: {
+            labels,
+            datasets: [{
+               backgroundColor: ({ dataIndex, dataset }) => {
+                  const val = <number>dataset.data[dataIndex];
+                  return `rgb(${val * 255 / this.frequency.mostFrequentChar.val / 100},0,0)`;
+               },
+               data,
+            }]
+         },
+         options: this.chartOptions,
+      });
    }
 
    private updatePairChart() {
 
+   }
+
+   private formatValue(value: number) {
+      return +(value * 100).toFixed(2);
    }
 
    private updateSummary() {
@@ -59,10 +93,10 @@ export default class Res {
 
    private getElements() {
       const r = this.root;
-      
+
       this.charCanvas = r.querySelector('.app-res__frequency-chart');
       this.pairCanvas = r.querySelector('.app-res__pairs-frequency-chart');
-      
+
       this.$.total = r.querySelector('.app-res__total');
       this.$.uniqueChars = r.querySelector('.app-res__unique-chars');
       this.$.uniquePairs = r.querySelector('.app-res__unique-pairs');
